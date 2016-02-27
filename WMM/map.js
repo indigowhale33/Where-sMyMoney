61086c8df50a1de71a06343f8ae98553209f1558
kdko <kdko@email.wm.edu>
2016-02-27 11:02:32 -0500

    HELP

--- WMM/map.js
+++ WMM/map.js
@@ -1,30 +1,62 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 3
-  });
+//var map;
+//var heatmap;
+//var heatMapData;
 
-  // Add marker on user click
-  map.addListener('click', function(e) {
-    firebase.push({lat: e.latLng.lat(), lng: e.latLng.lng()});
+function initMap(){
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: {lat: 38.881, lng: -77.247},
+    mapTypeId: google.maps.MapTypeId.MAP
   });
-
-  // Create a heatmap.
-  var heatmap = new google.maps.visualization.HeatmapLayer({
-    data: [],
-    map: map,
-    radius: 8
-  });
-
-  firebase.on("child_added", function(snapshot, prevChildKey) {
-    // Get latitude and longitude from Firebase.
-    var newPosition = snapshot.val();
-
-    // Create a google.maps.LatLng object for the position of the marker.
-    // A LatLng object literal (as above) could be used, but the heatmap
-    // in the next step requires a google.maps.LatLng object.
-    var latLng = new google.maps.LatLng(newPosition.lat, newPosition.lng);
-    
-    heatmap.getData().push(latLng);
-  });
+    var heatMapData = [];
+  	var heatmap = new google.maps.visualization.HeatmapLayer({
+  data: heatMapData
+});alert("hi");
+  	//process(map,heatmap, heatMapData);
 }
+
+
+function process(map, heatmap, heatMapData){
+      	alert("hi");
+      	    $(document).ready(function(){
+  			$.getJSON("http://api.reimaginebanking.com/accounts/56c66be6a73e492741507e65/purchases?key=d1964bf859fc21d67d2de4ca68e24dcd", function(data, status){
+  		var stringData = JSON.parse(JSON.stringify(data));
+  		for (var i = 0; i < stringData.length; i++) {
+
+  			$(document).ready(function(){
+  			
+  			var nestUrl = "http://api.reimaginebanking.com/merchants/" + stringData[i].merchant_id.toString() + 
+  				"?key=d1964bf859fc21d67d2de4ca68e24dcd";
+  			$.getJSON(nestUrl, function(data, status){
+     			// var latLng = new google.maps.LatLng(data.geocode.lat, data.geocode.lng);
+      		// 	var weightedLoc = {
+        // 		location: latLng,
+        // 		weight: 5;
+      		// 	};
+      		
+     			// heatMapData.push(weightedLoc);
+     			
+     		});
+  	});
+}
+
+  	});
+
+		
+	});
+	heatmap = new google.maps.visualization.HeatmapLayer({
+  data: heatMapData
+});
+	$(document).ready(function(){
+  	$.getJSON("http://api.reimaginebanking.com/customers/56c66be5a73e4927415073ca?key=d1964bf859fc21d67d2de4ca68e24dcd", function(data, status){
+     	document.getElementById("daname").innerHTML = JSON.parse(JSON.stringify(data)).first_name + " " + JSON.parse(JSON.stringify(data)).last_name;
+  	});
+		
+	});
+      }
+
+var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+
+
+heatmap.setMap(map);
